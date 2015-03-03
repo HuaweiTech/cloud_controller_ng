98329c1bdf42bfc63fa40c7bc7211e9a5f343c6a
AmitRoushan <amit.roushan@huawei.com>
2015-03-03 18:05:42 +0530

    Added prior state to app.update event
    Story: [#74624460]
    As per review comment , This fix is without meta-programming .
    Added a method prrio_state in app class which will decide and return prior
    state of app by analysing current state of app and changes returned by dirty plugin.
    
    Aditionally , same repository is getting used in ProcessesHandler class which passed AppProcess object after app update .
    So , added one instance variable prior state in  AppProcess class to get updated prior state of app.

--- spec/api/api_version_spec.rb
+++ spec/api/api_version_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'digest/sha1'
 
 describe 'Stable API warning system', api_version_check: true do
-  API_FOLDER_CHECKSUM = '0cae958c81a8951d625135ad42a344ffa5d7bd70'
+  API_FOLDER_CHECKSUM = 'd738c06dc82e622fca8c846afb87d11e7d90f095'
 
   it 'double-checks the version' do
     expect(VCAP::CloudController::Constants::API_VERSION).to eq('2.22.0')
